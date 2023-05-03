c8b907fb87bb164e0a390cee8c52ee1e0c1fabd8
juleshenry <julianplushenry@gmail.com>
2023-05-03 10:00:11 -0500

    /Users/mariopokemon/Desktop/projs/git-goblin/gacp

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # superwand
 Leverage magic wand bounding boxes to magically breath life to images
+
+
+![alt text](https://cdn.pixabay.com/photo/2012/04/24/17/48/magic-40641_1280.png)
+
+superwand --... yeah.... we still working on this
---
+++ transform-img.py
@@ -0,0 +1,54 @@
+class Img:
+	pass
+
+def flip(img: Img, dire="vertical") -> Img:
+	if dire =="vertical":
+		return
+	else dire = "horizontal":
+		return
+	else:
+		return img
+
+
+def clamp(o: float, mn=0,mx=1) -> o:
+	return (mn if (mx if o > mx else o) < mn else o)
+
+def gradient_boost(img: Img, style = "auto", completeness = "auto", opacity = "auto") -> Img:
+	"""
+	Converts monocolor regionswith directional gradient
+
+	style : 
+	~ direction of gradients
+		-> auto, best guess based on boosting
+		-> vertical
+		-> horizontal
+
+	completeness : 
+	~impacted regions
+		-> aggressive, all colors
+		-> auto, most impact
+		-> filter, color or set of colors
+	
+	opacity :
+	~makes achieve minimum opacity value in region
+		-> clamp {0, 1}
+		-> failsafe rounds
+	"""
+	if style == "auto":
+	else if style == "vertical":
+	else if style == "horizontal":
+	else:
+		style == "auto"
+
+	if completeness == "auto":
+	else if completeness == "aggressive":
+	else if completeness == "filter":
+	else: 
+		style == "auto"
+
+	opacity = clamp(opacity, 0, 1)
+
+
+	
+	return img
+
