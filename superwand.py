746558f43cab722c4a7ebcf4a1500d105bc6fd66
Julian Henry <julianplushenry@gmail.com>
2023-07-19 19:37:59 -0500

    /Users/mariopokemon/former/projs/git-goblin/gacp

---
+++ superwand.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import argparse
+
+def superwand(image: Image):
+    return
+
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Tatuagem")
+    st = "store_true"
+    parser.add_argument("--completeness", action=st, help="todo")  # fmt: skip
+    parser.add_argument("--style", action=st, help="todo")  # fmt: skip
+    parser.add_argument("--opacity", action=st, help="todo")  # fmt: skip
+     # Parse the first argument
+    args, positional_args = parser.parse_known_args()
+    image = positional_args[0]
+    # Access the option values
+    if args.completeness:
+        print("Text option is enabled")
+    if args.style:
+        print("Backsplash option is enabled")
+    if args.opacity:
+        print("Time stamp option is enabled")
+    superwand(image, **{a: getattr(args, a) for a in kwargs_list})
