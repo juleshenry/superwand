d73a9b1f8cdc284efb63dc20e6425b3530b1df81
Julian Henry <julianplushenry@gmail.com>
2024-03-16 22:59:08 -0500

    allahuakbar

--- region_identifier.py
+++ region_identifier.py
@@ -88,4 +88,20 @@ if __name__ == "__masin__":
         # break
             
 if __name__=='__main__':
+    # make new file. use `inject` to draw a circle on a blank image
+    new_image = Image.new("RGBA", (500, 500), (0, 0, 0, 0))
+    center = (250, 250)
+    radius = 100
+    color = (255, 0, 0)
+    circle_pixels = []
 
+    for x in range(center[0] - radius, center[0] + radius + 1):
+        for y in range(center[1] - radius, center[1] + radius + 1):
+            distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
+            if distance <= radius:
+                circle_pixels.append((x, y))
+
+    new_image = inject(new_image, circle_pixels, color)
+    new_image.save("circle_image.png")
+
+    
